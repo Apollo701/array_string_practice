8c8f1019b0ee41ffdd13529c6ca651867aeacac0
Jason Dorn <jdorn@jdorndev.com>
2015-01-07 17:24:14 -0800

    created permutation? method to check if one of two strings are subsets of each other

--- permuation.rb
+++ permuation.rb
@@ -1,5 +1,7 @@
-#This method will take two strings, and decide if one is a permuation of the other
+#This method will take two strings, and decide if one is a permutation of the other
 
 def permutation?(str1, str2)
-
+  true if ((str1.include? str2) || (str2.include? str1))
+else
+  false
 end
